2b3896a82200fa0a00f535698d885042060a08e7
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-07-23 19:58:20 +0900

    chore: install react-query-devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import type { AppProps } from 'next/app';
 const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <Component {...pageProps} />
     </QueryClientProvider>
   );

b8558e42627f52b530e0d649de5889f65923e6be
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-07-25 00:35:24 +0900

    feat: add style to tab

--- src/features/todos/components/FilterButtons/FilterLinks.tsx
+++ src/features/todos/components/FilterButtons/FilterLinks.tsx
@@ -1,20 +1,52 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 
-import { FILTER_NAMES } from '../';
+import { FILTER_NAMES, TFilterName } from '../';
 import style from './FilterLinks.module.css';
 
 export const FilterLinks: FC = () => {
+  const router = useRouter();
+  const filter = router.query.filter as TFilterName;
+
+  const isAllSelected = filter === FILTER_NAMES.ALL || filter === undefined;
+  const isActiveSelected = filter === FILTER_NAMES.ACTIVE;
+  const isCompletedSelected = filter === FILTER_NAMES.COMPLETED;
+
   return (
-    <div className={style.root}>
+    <div className={style.root} role="tablist">
       <Link href={`?filter=${FILTER_NAMES.ALL}`}>
-        <a>All</a>
+        <a
+          role="tab"
+          aria-selected={isAllSelected}
+          aria-controls="panel-1"
+          id="tab-1"
+          tabIndex={isAllSelected ? 0 : -1}
+        >
+          All
+        </a>
       </Link>
       <Link href={`?filter=${FILTER_NAMES.ACTIVE}`}>
-        <a>Active</a>
+        <a
+          role="tab"
+          aria-selected={isActiveSelected}
+          aria-controls="panel-2"
+          id="tab-1"
+          tabIndex={isActiveSelected ? 0 : -1}
+        >
+          Active
+        </a>
       </Link>
       <Link href={`?filter=${FILTER_NAMES.COMPLETED}`}>
-        <a>Completed</a>
+        <a
+          role="tab"
+          aria-selected={isCompletedSelected}
+          aria-controls="panel-3"
+          id="tab-1"
+          tabIndex={isCompletedSelected ? 0 : -1}
+        >
+          Completed
+        </a>
       </Link>
     </div>
   );

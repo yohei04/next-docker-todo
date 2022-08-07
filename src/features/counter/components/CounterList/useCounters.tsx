649b5f6683dbe4cfb93bd82c85abf4f3aec1c971
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-08-07 19:43:29 +0900

    refactor: use render patern

--- src/features/counter/components/CounterList/useCounters.tsx
+++ src/features/counter/components/CounterList/useCounters.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from 'react';
+import { FC, useCallback, useMemo, useState } from 'react';
 
 export type TCounter = {
   label: string;
@@ -13,62 +13,77 @@ type TUseCounters = {
   incrementByTwo: (index: number) => void;
 };
 
-export const useCounters = (initialCounters: TCounter[]): TUseCounters => {
-  const [counters, setCounters] = useState<TCounter[]>(initialCounters);
+export const useCounters = ({ label: initialLabel, value: initialValue }: TCounter) => {
+  // const [counters, setCounters] = useState<TCounter[]>(initialCounters);
 
-  const countNum = useMemo(() => counters.reduce((acc, cur) => acc + cur.value, 0), [counters]);
+  // const countNum = useMemo(() => counters.reduce((acc, cur) => acc + cur.value, 0), [counters]);
 
-  // strictモードでも正しく動く
-  const increment = useCallback((index: number) => {
-    console.log('run once?');
-    setCounters((prev) => {
-      console.log('run twice?');
-      const newCounters = prev.map((counter, i) => {
-        if (i === index) {
-          return {
-            ...counter,
-            value: counter.value + 1,
-          };
-        }
-        return counter;
-      });
-      return newCounters;
-    });
-  }, []);
+  // // strictモードでも正しく動く
+  // const increment = useCallback((index: number) => {
+  //   console.log('run once?');
+  //   setCounters((prev) => {
+  //     console.log('run twice?');
+  //     const newCounters = prev.map((counter, i) => {
+  //       if (i === index) {
+  //         return {
+  //           ...counter,
+  //           value: counter.value + 1,
+  //         };
+  //       }
+  //       return counter;
+  //     });
+  //     return newCounters;
+  //   });
+  // }, []);
 
-  const decrement = useCallback((index: number) => {
-    setCounters((prev) => {
-      // strictモードでも正しく動く
-      const newCounters = structuredClone(prev);
-      // strictモードで壊れる（shallow copyでミュータブルなため）
-      // const newCounters = [...prev];
-      newCounters[index].value -= 1;
-      return newCounters;
-    });
-  }, []);
+  // const decrement = useCallback((index: number) => {
+  //   setCounters((prev) => {
+  //     // strictモードでも正しく動く
+  //     const newCounters = structuredClone(prev);
+  //     // strictモードで壊れる（shallow copyでミュータブルなため）
+  //     // const newCounters = [...prev];
+  //     newCounters[index].value -= 1;
+  //     return newCounters;
+  //   });
+  // }, []);
 
-  const incrementByTwo = useCallback((index: number) => {
-    console.log('run once?');
-    setCounters((prev) => {
-      console.log('run twice?');
-      const newCounters = prev.map((counter, i) => {
-        if (i === index) {
-          return {
-            ...counter,
-            value: counter.value + 2,
-          };
-        }
-        return counter;
-      });
-      return newCounters;
-    });
-  }, []);
+  // const incrementByTwo = useCallback((index: number) => {
+  //   console.log('run once?');
+  //   setCounters((prev) => {
+  //     console.log('run twice?');
+  //     const newCounters = prev.map((counter, i) => {
+  //       if (i === index) {
+  //         return {
+  //           ...counter,
+  //           value: counter.value + 2,
+  //         };
+  //       }
+  //       return counter;
+  //     });
+  //     return newCounters;
+  //   });
+  // }, []);
+
+  const [value, setValue] = useState(initialValue);
+  const increment = useCallback(() => setValue((prev) => prev + 1), []);
+  const incrementByTwo = useCallback(() => setValue((prev) => prev + 2), []);
+  const decrement = useCallback(() => setValue((prev) => prev - 1), []);
+
+  const counterItem = (
+    <>
+      <h3>共通タイトル</h3>
+      <div>{initialLabel}</div>
+      <div>{value}</div>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+        {incrementByTwo && <button onClick={incrementByTwo}>+2</button>}
+      </div>
+    </>
+  );
 
   return {
-    counters,
-    countNum,
-    increment,
-    decrement,
-    incrementByTwo,
+    value,
+    counterItem,
   };
 };

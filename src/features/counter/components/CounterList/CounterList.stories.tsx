e08b564d626a632ba9784e7ce9712469db2ac12e
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-08-18 00:08:34 +0900

    test: add counter test

--- src/features/counter/components/CounterList/CounterList.stories.tsx
+++ src/features/counter/components/CounterList/CounterList.stories.tsx
@@ -28,3 +28,36 @@ export const IncrementCounter: ComponentStoryObj<typeof CounterList> = {
     expect(counterItemValue).toHaveTextContent('1');
   },
 };
+
+export const DecrementCounter: ComponentStoryObj<typeof CounterList> = {
+  ...Default,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const decrementButton = canvas.getByRole('button', { name: 'counter-decrement-1' });
+    await userEvent.click(decrementButton);
+    const counterItemValue = canvas.getByLabelText('counter-value-1');
+    expect(counterItemValue).toHaveTextContent('-1');
+  },
+};
+
+export const AddCounter: ComponentStoryObj<typeof CounterList> = {
+  ...Default,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const addButton = canvas.getByRole('button', { name: 'カウンターを追加' });
+    await userEvent.click(addButton);
+    const counters = canvas.getAllByRole('listitem');
+    expect(counters).toHaveLength(3);
+  },
+};
+
+export const DeleteCounter: ComponentStoryObj<typeof CounterList> = {
+  ...Default,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const deleteButtons = canvas.getAllByRole('button', { name: '削除' });
+    await userEvent.click(deleteButtons[0]);
+    const counters = canvas.getAllByRole('listitem');
+    expect(counters).toHaveLength(1);
+  },
+};

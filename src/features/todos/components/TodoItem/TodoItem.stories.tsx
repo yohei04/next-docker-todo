b8d132db4f951c546cedc132df34bf96eb14513e
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-08-20 09:02:49 +0900

    feat: add TodoItem edit story

--- src/features/todos/components/TodoItem/TodoItem.stories.tsx
+++ src/features/todos/components/TodoItem/TodoItem.stories.tsx
@@ -20,3 +20,33 @@ export const Default: ComponentStoryObj<typeof TodoItem> = {
     },
   },
 };
+
+export const Editing: ComponentStoryObj<typeof TodoItem> = {
+  ...Default,
+  name: '編集中',
+  parameters: {
+    docs: {
+      description: {
+        story: '編集ボタン押下後は元のnameがinputに表示される',
+      },
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const editButton = canvas.getByRole('button', { name: '編集' });
+    await userEvent.click(editButton);
+    const editInput = canvas.getByLabelText('edit-todo-input');
+    expect(editInput).toHaveValue('テストTodo');
+  },
+};
+
+export const Update: ComponentStoryObj<typeof TodoItem> = {
+  ...Default,
+  play: async ({ canvasElement }) => {
+    await Editing.play?.({ canvasElement });
+    const canvas = within(canvasElement);
+    const editInput = canvas.getByLabelText('edit-todo-input');
+    await userEvent.type(editInput, '変更しました');
+    expect(editInput).toHaveValue(`${Default.args?.todo?.name}変更しました`);
+  },
+};
